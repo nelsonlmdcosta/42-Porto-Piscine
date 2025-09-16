5f01ac91b7a8aafffdd57a0e4a5124647043d412
Nelson Luis Moreira da Costa <nelsonlmdcosta@gmail.com>
2025-09-16 07:19:00 +0100

    Fixed a possible memory leak on certain errors for C_08

--- C_08/ex05/test_ex05.c
+++ C_08/ex05/test_ex05.c
@@ -6,20 +6,44 @@
 /*   By: nluis-mo <nluis-mo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 07:50:40 by nluis-mo          #+#    #+#             */
-/*   Updated: 2025/09/13 12:40:15 by nluis-mo         ###   ########.fr       */
+/*   Updated: 2025/09/15 22:00:12 by nluis-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <stdlib.h>
 #include "ft_stocks_str.h"
 
+/* Prototypes from your previous exercises */
 t_stock_str	*ft_strs_to_tab(int ac, char **av);
 void		ft_show_tab(struct s_stock_str *par);
 
-int	main(int ac, char **av)
-{
-	t_stock_str	*tab;
+/* Optional: helpers from previous exercises (ft_putstr, ft_putnbr) */
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
 
-	tab = ft_strs_to_tab(ac, av);
+int	main(void)
+{
+	char *strings[] = {
+			"Hello",
+			"42",
+			"C programming",
+			"Test string"
+	};
+	int count = 4;
+
+	/* Convert array of strings to t_stock_str array */
+	t_stock_str *tab = ft_strs_to_tab(count, strings);
+	if (!tab)
+		return (1);
+
+	/* Show the content of the tab */
 	ft_show_tab(tab);
+
+	/* Free the allocated copies */
+	for (int i = 0; i < count; i++)
+		free(tab[i].copy);
+	free(tab);
+
 	return (0);
 }
