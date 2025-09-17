f2fd788cfebb70dac946935bc34f1ff72859d91b
nluis-mo <nluis-mo@student.42porto.com>
2025-09-17 16:07:19 +0100

    Added files for fixes to C08

--- C_08/ex04/ft_stock_str.h
+++ C_08/ex04/ft_stock_str.h
@@ -6,12 +6,12 @@
 /*   By: nluis-mo <nluis-mo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 12:28:14 by nluis-mo          #+#    #+#             */
-/*   Updated: 2025/09/13 12:36:44 by nluis-mo         ###   ########.fr       */
+/*   Updated: 2025/09/13 12:29:14 by nluis-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_STOCKS_STR_H
-# define FT_STOCKS_STR_H
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
 
 typedef struct s_stock_str
 {
